5ebc9f0360e984636f979ac27e9f826e221fcdf6
Danny Soppit <55635377+DanSop@users.noreply.github.com>
2025-01-29 13:18:49 -0700

    Delete js directory

--- js/script.js
+++
@@ -1,8 +0,0 @@
-title: My Minimalistic Portfolio
-description: A clean and minimalistic website for my articles.
-baseurl: ""
-url: "https://username.github.io"
-markdown: kramdown
-highlighter: rouge
-plugins:
-  - jekyll-feed
